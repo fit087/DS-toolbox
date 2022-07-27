79da681d71822f19be43288ebd5062fbf8f8aa25
adolfo <adolfo.correa@br.ey.com>
2022-07-26 22:50:14 -0300

    objects introduction

--- js/aula2_funcao_e_objeto.js
+++ js/aula2_funcao_e_objeto.js
@@ -5,7 +5,7 @@ var siglas = ['bh', 'sp', 'poa'];
 console.log(siglas[0].toUpperCase());
 
 function acertarSigla(sigla){
-    var sigla_final = sigla.toUpperCase();
+    var sigla_final = sigla.toUpperCase().split('-')[0];
     return sigla_final;
 }
 
@@ -15,6 +15,34 @@ console.log(acertarSigla(siglas[2]));
 
 console.log(acertarSigla('bh-mg'));
 
+// Callback?
+// Funcao anonima
+// execucao com delay
+
+setTimeout(function(){
+    console.log("Atrasado ...");
+}, 5000)
+
+
+
 // Objetos - https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Working_with_Objects
 
 
+var pessoa = {
+    "nome": "Marcelo Sabadini",
+    "idade": 37,
+    "instrumentos": ["Guitarra", "Bateria"],
+    "filhos": [
+        {
+            "nome": "Giovana",
+            "idade": 6
+        },
+        {
+            "nome": "Israel",
+            "idade": 8
+        }
+    ]
+}
+console.log(typeof(pessoa))
+
+console.log(pessoa)
